390bbcfb253f249c2e6af96f7fc8e34ec804624b
Luis Aragon <laragon@dinterweb.com>
2025-03-26 16:53:22 -0600

    Agregando vista y ruta para mostrar Registros del Objeto Personalizado Mascota

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_ACCESS_TOKEN;
+const CUSTOM_OBJECT_NAME = process.env.CUSTOM_OBJECT_NAME;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_NAME}?properties=name,species,race`, {
+            headers: {
+                Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = response.data.results;
+        res.render('homepage', { title: 'Mascotas', data });
+    } catch (error) {
+        console.error('Error al obtener datos:', error.response?.data || error.message);
+        res.status(500).send('Error al obtener datos de mascotas');
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
